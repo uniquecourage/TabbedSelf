179dfa25c375368e4e732d7976aea4bd439621b6
uniquecourage <garrick@linjinlongdeMacBook-Pro.local>
2015-09-01 16:32:46 +0800

    20150901公司上傳

---
+++ TabbedSelf/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedSelf
+//
+//  Created by 林金龍 on 2015/9/1.
+//  Copyright (c) 2015年 林金龍. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TabbedSelf/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedSelf
+//
+//  Created by 林金龍 on 2015/9/1.
+//  Copyright (c) 2015年 林金龍. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
