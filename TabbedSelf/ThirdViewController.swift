4c92bafd8caa336b1fdf6f4fe0fd746ab9fcc169
uniquecourage <garrick@linjinlongdeMacBook-Pro.local>
2015-09-01 17:24:51 +0800

    20150901(2) 公司上傳

---
+++ TabbedSelf/ThirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdViewController.swift
+//  TabbedSelf
+//
+//  Created by 林金龍 on 2015/9/1.
+//  Copyright (c) 2015年 林金龍. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
